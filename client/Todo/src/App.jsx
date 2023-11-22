4607ecc36388a91f7c0262c85c9ac43edf84a679
lokeshirl <lokeshkumarjha07+github@gmail.com>
2023-11-22 15:13:21 +0530

    initial setup client/Todo (frontend)

---
+++ client/Todo/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+const App = () => {
+  return (
+    <Button variant="contained">Hello world</Button>
+  )
+}
+
+export default App
